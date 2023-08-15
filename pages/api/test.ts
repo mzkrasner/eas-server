0b64be240f1ee2a014312779a87ef67c77b27a11
Mark Krasner <markkrasner@Marks-MacBook.local>
2023-08-15 10:57:54 -0400

    changing return logic

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-import {dynamicCreate} from '../../scripts/comp2.mjs'
+import { dynamicCreate } from "../../scripts/comp2.mjs";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-      const hash = req.body
-      console.log(hash.toString())
-      const stream = await dynamicCreate(hash)
-      res.status(200).json({ stream });
+  const hash = req.body;
+  console.log(hash.toString());
+  const stream = await dynamicCreate(hash);
+  if (stream) {
+    res.status(200).json({ stream });
+  }
 };
